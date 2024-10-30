6c2242d62d7abefa78f9472583b36e76fca804da
Thomas Capelle <tcapelle@pm.me>
2024-10-30 22:46:17 +0100

    save load JSONL

--- evalforge/utils.py
+++ evalforge/utils.py
@@ -1,4 +1,5 @@
 import json
+from pathlib import Path
 
 def pprint(d, indent=4):
     """Pretty print a dictionary or other object."""
@@ -6,3 +7,15 @@ def pprint(d, indent=4):
         print(json.dumps(d, indent=indent))
     else:
         print(d)
+
+def load_jsonl(filename: Path | str) -> list[dict]:
+    """Load a JSONL file into a list of dictionaries."""
+    with open(filename, "r") as file:
+        return [json.loads(line) for line in file]
+
+def save_jsonl(data: list[dict], filename: Path | str):
+    """Save a list of dictionaries to a JSONL file."""
+    with open(filename, "w") as file:
+        for example in data:
+            json.dump(example, file)
+            file.write("\n")
